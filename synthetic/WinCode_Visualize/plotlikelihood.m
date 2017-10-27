312fc39d3b5ee2efe3ae81ea3eef5362eb93f1f2
simayuhe <1281139679@qq.com>
2017-10-27 17:44:09 +0800

    SOME CHANE I DIDNOT FIND

--- synthetic/WinCode_Visualize/plotlikelihood.m
+++ synthetic/WinCode_Visualize/plotlikelihood.m
@@ -4,6 +4,6 @@ clear all;
 clc
 %for i=1:1:4
    % figure(i)
-    load(['../result/experiter1000/likelihoods.txt'])
+    load(['../result/exper30304/likelihoods.txt'])
     plot([1:size(likelihoods,2)], likelihoods,'-')
 %end
